43cd561c2b95bfdf9a10b2947be858444ca9e3f6
Liam Rust <liam@loim.dev>
2026-07-13 04:14:32 -0400

    css cleanup & editor sizing fix

--- uno.config.ts
+++ uno.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig, presetWind4 } from "unocss";
+import warmBurnoutDark from "./setup/warm-burnout-dark.json";
+import warmBurnoutLight from "./setup/warm-burnout-light.json";
 
 const shade = (name: string) =>
   Object.fromEntries(
@@ -12,7 +14,10 @@ export default defineConfig({
   theme: {
     colors: {
       rust: shade("rust"),
-      taupe: shade("taupe"), // to make sure all shades generate
+      shikibg: {
+        light: warmBurnoutLight.colors["editor.background"],
+        dark: warmBurnoutDark.colors["editor.background"],
+      },
     },
     // because the headmatter doesn't set it correctly...
     font: {
